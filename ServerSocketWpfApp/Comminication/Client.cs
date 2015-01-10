d102971a65931a79da7eda0392a90d036a053800
Piotr <piotrr.malecki@gmail.com>
2015-01-10 15:06:33 +0100

    initial commit

---
+++ ServerSocketWpfApp/Comminication/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Comminication
+{
+    class ClientConnect
+    {
+        public String type { get; set; }
+
+        public List<Client> list = new List<Client>();
+
+    }
+    public class Client
+    {
+        public int id { get; set; }
+
+        public String name { get; set; }
+        public Client(int _id, String _name)
+        {
+            id = _id;
+            name = _name;
+        }
+    }
+}
---
+++ WpfApplication1/Comminication/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Comminication
+{
+    class ClientConnect
+    {
+        public String type { get; set; }
+
+        public List<Client> list = new List<Client>();
+        public ClientConnect(String _type, List<Client> _list)
+        {
+            type = _type;
+            list = _list;
+        }
+
+    }
+    public class Client
+    {
+        public String id { get; set; }
+
+        public String name { get; set; }
+        public Client(String _id, String _name)
+        {
+            id = _id;
+            name = _name;
+        }
+    }
+}
